f888e9719b43c00add9c346d6fb50024207fc22f
Yaxiong Hu <1006738657@qq.com>
2019-07-23 15:18:07 -0700

    new changes

--- Java/nullCast/NullNumbers.java
+++ Java/nullCast/NullNumbers.java
@@ -1,38 +1,52 @@
 import java.math.BigInteger;
+import java.util.List;
 
 public class NullNumbers {
     public static void main(String[] args) {
         try {
-	    String num = "123";
-	    BigInteger bi = new BigInteger(num);
-	    System.out.println("bi: " + bi);
-	} catch(Exception e) {
-	     e.printStackTrace();
-	}
+			String num = "123";
+			BigInteger bi = new BigInteger(num);
+			System.out.println("bi: " + bi);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
-	try {
-	    String num = null;
-	    BigInteger bi = new BigInteger(num);
-	    System.out.println("bi: " + bi);
-	} catch(Exception e) {
-	     e.printStackTrace();
-	}
+		try {
+			String num = null;
+			BigInteger bi = new BigInteger(num);
+			System.out.println("bi: " + bi);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
-	try {
-	     Long l = null;
-	     long ll = l;
-	     System.out.println("long: " + ll);
-	} catch(Exception e) {
-	     System.out.println("when parse long");
-	     e.printStackTrace();
-	}
+		try {
+			Long l = null;
+			long ll = l;
+			System.out.println("long: " + ll);
+		} catch(Exception e) {
+			System.out.println("when parse long");
+			e.printStackTrace();
+		}
 
-	try {
-	     Long l = null;
-	     System.out.println("can print null Long " + l);
-	} catch(Exception e) {
-	     System.out.println("can not print null Long");
-	     e.printStackTrace();
+		try {
+			Long l = null;
+			System.out.println("can print null Long " + l);
+		} catch(Exception e) {
+			System.out.println("can not print null Long");
+			e.printStackTrace();
+		}
+
+		try {
+			List<String> lst = null;
+			for (String s : lst) {
+				System.out.println("here?");
+			}
+			System.out.println("do not throw nullpointer");
+		} catch(Exception e) {
+			System.out.println("nullpointer ?");
+			e.printStackTrace();
+		}
+
+		
 	}
-    }
 }
